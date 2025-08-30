fe68d3b5aeb68b1e8ddb74e6a882482430beb723
Dilmi Tharushika <Dilmi.MerenchiKankanamge@metropolia.fi>
2025-08-30 20:00:06 +0300

    First commit

--- Orientation_Task_1_3/src/Test_program.java
+++ Orientation_Task_1_3/src/Test_program.java
@@ -1,2 +1,19 @@
 public class Test_program {
+    public static void main(String[] args) {
+
+        Customer c1 = new Customer();
+        c1.setStartTime(10);
+        c1.setEndTime(30);
+        System.out.println("Customer " + c1.getId() + " spent " + c1.getTimeSpent() + " units of time.");
+
+        Customer c2 = new Customer();
+        c2.setStartTime(5);
+        c2.setEndTime(12);
+        System.out.println("Customer " + c2.getId() + " spent " + c2.getTimeSpent() + " units of time.");
+
+        Customer c3 = new Customer();
+        c3.setStartTime(100);
+        c3.setEndTime(250);
+        System.out.println("Customer " + c3.getId() + " spent " + c3.getTimeSpent() + " units of time.");
+    }
 }
